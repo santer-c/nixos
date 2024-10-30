0da9dade951911b140080aa9fe8e76f924c887f8
santer <114699148+santer-c@users.noreply.github.com>
2024-10-30 18:25:19 -0400

    nixos directory files

---
+++ nixos/packages.nix
@@ -0,0 +1,57 @@
+{ pkgs, ... }: {
+  nixpkgs.config = {
+    allowUnfree = true;
+  };
+
+  environment.systemPackages = with pkgs; [
+    # Desktop apps
+    alacritty
+    rofi
+
+    # Coding stuff
+    gnumake
+    gcc
+
+    # CLI utils
+    fastfetch
+    file
+    tree
+    wget
+    git
+    htop
+    nix-index
+    unzip
+    ranger
+    zip
+    yt-dlp
+    lazygit
+
+    # GUI utils
+    feh
+    dmenu
+
+    # WMs and stuff
+    i3status
+    i3lock
+    i3
+
+    # Sound
+    pipewire
+    pulseaudio
+    pamixer
+
+    # Other
+    home-manager
+  ];
+
+  fonts.packages = with pkgs; [
+    jetbrains-mono
+    noto-fonts
+    noto-fonts-emoji
+    twemoji-color-font
+    font-awesome
+    powerline-fonts
+    powerline-symbols
+    (nerdfonts.override { fonts = [ "NerdFontsSymbolsOnly" ]; })
+  ];
+}
