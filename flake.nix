2833141c9dc1334b0f4844c3e0e8d66623a4517e
santer <114699148+santer-c@users.noreply.github.com>
2024-11-01 16:19:46 -0400

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
 
     home-manager = {
-      url = "github:nix-community/home-manager/archive/release-24.05.tar.gz";
+      url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
